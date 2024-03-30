ed402ce22271974599d21712ad65f1ff6a2ccaf2
shun suzuki <suzuki@hapis.k.u-tokyo.ac.jp>
2024-03-30 09:10:43 +0900

    fix bench

--- benches/holo.rs
+++ benches/holo.rs
@@ -1,14 +1,8 @@
 #[cfg(feature = "bench-utilities")]
 criterion::criterion_group!(
     benches,
-    autd3_gain_holo::test_utilities::bench_utils::foci::<
-        autd3_backend_arrayfire::ArrayFireBackend,
-        4,
-    >,
-    autd3_gain_holo::test_utilities::bench_utils::devices::<
-        autd3_backend_arrayfire::ArrayFireBackend,
-        2,
-    >
+    autd3_gain_holo::bench_utilities::foci::<autd3_backend_arrayfire::ArrayFireBackend, 4>,
+    autd3_gain_holo::bench_utilities::devices::<autd3_backend_arrayfire::ArrayFireBackend, 2>
 );
 #[cfg(feature = "bench-utilities")]
 criterion::criterion_main!(benches);
